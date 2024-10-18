b66a154524050008b16413121318e33b65c11dd1
Nga Vu <134727438+zlnga1001@users.noreply.github.com>
2024-10-18 15:07:49 -0400

    Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -37,7 +37,12 @@ export default function Card() {
         <h3 className="back">Chúc mừng sinh nhật tuổi 20!</h3>
         <p>Hiếu thân mến,</p>
         <p>
-        Time flies, doesn’t it? In just the blink of an eye, you've already turned 20, the age that, for me, is a milestone to reflect on how much I've changed over the past 20 years. I know that you have reached many milestones—there may have been regrets, but also happy achievements during that time—but life is all about choices, right? I hope that as you pass this age of 20, the things you wish for will come to you, the things you haven’t yet accomplished will become a reality, and the things you have done will become beautiful memories in your mind. I hope that on the journey ahead, you will live to the fullest, be joyful, stay healthy, and find happiness. Happy birthday, Nguyễn Đình Hiếu.
+        Time flies, doesn’t it? In just the blink of an eye, you've already turned 20, the age that, for me, 
+          is a milestone to reflect on how much we've changed over the past 20 years. I know that you have reached 
+          many milestones—there may have been regrets, but also happy achievements during that time. I hope that as you 
+          pass this age of 20, the things you wish for will come to you, the things you 
+          haven’t yet accomplished will become a reality, and the things you have done will become beautiful memories in your mind. 
+          I hope that on the journey ahead, you will live to the fullest, be joyful, stay healthy. Happy birthday, Nguyễn Đình Hiếu.
         </p>
         <p className="name">Vũ Linh Nga</p>
       </div>
